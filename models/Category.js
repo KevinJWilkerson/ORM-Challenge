7340c39e6cdd292e35f868c224df3036fbd49fe7
Kevin <kevin.j.wilkerson@gmail.com>
2022-04-20 09:46:30 -0500

    set up initial repo structure

---
+++ models/Category.js
@@ -0,0 +1,18 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
